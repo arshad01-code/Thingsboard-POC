569985980bed54bb380ee9c4aa52fdff50df0349
arshad01-code <arshadabdullah01@gmail.com>
2026-02-20 17:49:48 +0000

    Http change: 1. Changed from mqtt to http.

--- app/device_manager.py
+++ app/device_manager.py
@@ -1,5 +1,6 @@
 import json, time
-from app.config import DEVICE_TOKENS, MAX_RETRIES
+import requests
+from app.config import DEVICE_TOKENS, MAX_RETRIES, BROKER
 from app.mqtt_client import create_client
 from app.telemetry import generate_telemetry
 from app.retry import retry_execution
@@ -7,6 +8,7 @@ from app.retry import retry_execution
 def send_device_data(token: str, device_index: int):
     device_name = f"Device-{device_index}"
 
+    """
     def publish():
         client = create_client(
             token=token
@@ -27,6 +29,22 @@ def send_device_data(token: str, device_index: int):
         print(f"SUCCESS: {device_name} -> {data}")
         client.disconnect()
         print("Finished.")
+    """    
+
+    def publish(device_name, token):
+        url = f"https://{BROKER}/api/v1/{token}/telemetry"
+        data = generate_telemetry(
+            device_name=device_name
+        )
+        print(f"{device_name}: Sending payload {data}", flush=True)
+        try:
+            response = requests.post(url, json=data, timeout=10)
+            if response.status_code == 200:
+                print(f"{device_name}: SUCCESS -> {data} (HTTP 200)", flush=True)
+            else:
+                print(f"{device_name}: FAILED -> {data} (HTTP {response.status_code})", flush=True)
+        except Exception as e:
+            print(f"{device_name}: ERROR -> {e}", flush=True)
 
     retry_execution(publish, MAX_RETRIES)
 
