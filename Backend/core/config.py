9b37b16b8a09615e7f9f45ebfc3ba6df531cb12f
arshad01-code <arshadabdullah01@gmail.com>
2026-02-21 21:20:02 +0000

    2. Changed the secret name to JWsecret.

--- Backend/core/config.py
+++ Backend/core/config.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = os.getenv("JWT_SECRET")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_HOURS = 12
